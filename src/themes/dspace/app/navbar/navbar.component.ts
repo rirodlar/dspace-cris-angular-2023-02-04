d134c9754c6ad3e5448ff813956e527a284dda43
rarodriguezl <ricardo.rodriguez.lara@gmail.com>
2024-12-04 19:54:59 -0300

    feat: navbar

--- src/themes/dspace/app/navbar/navbar.component.ts
+++ src/themes/dspace/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavbarComponent as BaseComponent } from '../../../../app/navbar/navbar.component';
 import { slideMobileNav } from '../../../../app/shared/animations/slide';
 
@@ -9,7 +9,25 @@ import { slideMobileNav } from '../../../../app/shared/animations/slide';
   selector: 'ds-navbar',
   styleUrls: ['./navbar.component.scss'],
   templateUrl: './navbar.component.html',
-  animations: [slideMobileNav]
+  animations: [slideMobileNav],
 })
 export class NavbarComponent extends BaseComponent {
+  isSubmenuOpen = false;
+
+  toggleSubmenu(event: Event) {
+    event.preventDefault();
+    this.isSubmenuOpen = !this.isSubmenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeSubmenu(event: MouseEvent) {
+    const clickedInside =
+      event.target instanceof HTMLElement &&
+      (event.target.closest('.nav-item') ||
+        event.target.closest('.dropdown-menu'));
+
+    if (!clickedInside) {
+      this.isSubmenuOpen = false;
+    }
+  }
 }
