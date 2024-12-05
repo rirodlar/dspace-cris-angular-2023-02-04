3fc5a7b582791c0a7bc51e02a9d0d3cb55a81521
rarodriguezl <ricardo.rodriguez.lara@gmail.com>
2024-12-05 12:35:30 -0300

    feat: navbar

--- src/themes/dspace/app/navbar/navbar.component.ts
+++ src/themes/dspace/app/navbar/navbar.component.ts
@@ -11,23 +11,4 @@ import { slideMobileNav } from '../../../../app/shared/animations/slide';
   templateUrl: './navbar.component.html',
   animations: [slideMobileNav],
 })
-export class NavbarComponent extends BaseComponent {
-  isSubmenuOpen = false;
-
-  toggleSubmenu(event: Event) {
-    event.preventDefault();
-    this.isSubmenuOpen = !this.isSubmenuOpen;
-  }
-
-  @HostListener('document:click', ['$event'])
-  closeSubmenu(event: MouseEvent) {
-    const clickedInside =
-      event.target instanceof HTMLElement &&
-      (event.target.closest('.nav-item') ||
-        event.target.closest('.dropdown-menu'));
-
-    if (!clickedInside) {
-      this.isSubmenuOpen = false;
-    }
-  }
-}
+export class NavbarComponent extends BaseComponent {}
